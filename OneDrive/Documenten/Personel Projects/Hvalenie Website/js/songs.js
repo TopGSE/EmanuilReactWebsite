725858066e8be69a261e4a52523fc5cf69c11a95
TopGSE <solomon.ekov@gmail.com>
2024-09-23 21:55:10 +0200

    Clean up unwanted files and update .gitignore

---
+++ OneDrive/Documenten/Personel Projects/Hvalenie Website/js/songs.js
@@ -0,0 +1,79 @@
+const songs = [
+    {
+		title: "Amazing Grace",
+		lyrics: `
+		Amazing grace, how sweet the sound
+		That saved a wretch like me
+		I once was lost, but now I'm found
+		Was blind, but now I see.
+	
+		'Twas grace that taught my heart to fear
+		And grace my fears relieved
+		How precious did that grace appear
+		The hour I first believed.
+	
+		Through many dangers, toils, and snares
+		I have already come
+		'Tis grace has brought me safe thus far
+		And grace will lead me home.
+	
+		The Lord has promised good to me
+		His word my hope secures
+		He will my shield and portion be
+		As long as life endures.
+	
+		Yea, when this flesh and heart shall fail
+		And mortal life shall cease
+		I shall possess within the veil
+		A life of joy and peace.
+	
+		When we've been there ten thousand years
+		Bright shining as the sun
+		We've no less days to sing God's praise
+		Than when we first begun.`,
+		
+		chords: `
+		[G] Amazing grace, how [C] sweet the [G] sound
+		That saved a wretch like [D] me
+		[G] I once was lost, but [C] now I'm [G] found
+		Was [C] blind, but [G] now [D] I [G] see.
+	
+		'Twas [G] grace that taught my [C] heart to [G] fear
+		And grace my fears re[D]lieved
+		[G] How precious did that [C] grace ap[G]pear
+		The [C] hour I [G] first [D] be[G]lieved.
+	
+		Through [G] many dangers, [C] toils, and [G] snares
+		I have already [D] come
+		[G] 'Tis grace has brought me [C] safe thus [G] far
+		And [C] grace will [G] lead [D] me [G] home.
+	
+		The [G] Lord has promised [C] good to [G] me
+		His word my hope se[D]cures
+		[G] He will my shield and [C] portion [G] be
+		As [C] long as [G] life [D] en[G]dures.
+	
+		Yea, [G] when this flesh and [C] heart shall [G] fail
+		And mortal life shall [D] cease
+		[G] I shall possess, with[C]in the [G] veil
+		A [C] life of [G] joy and [D] peace.
+	
+		When [G] we've been there ten [C] thousand [G] years
+		Bright shining as the [D] sun
+		[G] We've no less days to [C] sing God's [G] praise
+		Than [C] when we [G] first [D] be[G]gun.`
+    },
+    {
+        title: "How Great Thou Art",
+        lyrics: `
+        O Lord my God, when I in awesome wonder
+        Consider all the worlds Thy Hands have made
+        I see the stars, I hear the rolling thunder
+        Thy power throughout the universe displayed.`,
+        chords: `
+        [C] O Lord my God, when [F] I in awesome [C] wonder
+        Consider all the [G] worlds Thy Hands have [C] made
+        I see the stars, I [F] hear the rolling [C] thunder
+        Thy power through[G]out the [C] universe dis[G]played.`
+    }
+];
